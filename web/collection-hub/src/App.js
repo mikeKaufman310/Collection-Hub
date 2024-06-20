de30e0a2b2f94f5065236358fd396458d6616538
mikeKaufman310 <mpk225@lehigh.edu>
2024-06-20 13:50:38 -0400

    started experimenting with react frontend interacting with springboot backend and rendering data

--- web/collection-hub/src/App.js
+++ web/collection-hub/src/App.js
@@ -1,9 +1,38 @@
+import {useEffect, useState} from 'react';
+
+const BACKEND_PORT = 8080;
+
 export default function App(){
   return(
       <div>
           <h1>
               Welcome to CollectionHub!
           </h1>
+          <CollectionButton/>
       </div>
+  );
+}
+
+
+function CollectionButton(){
+  const [data, setData] = useState([]);
+  
+  useEffect(()=>{
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', `http://localhost:${BACKEND_PORT}/allCollections`);
+  xhr.onload = function() {
+    if (xhr.status === 200) {
+      setData(JSON.parse(xhr.responseText));
+    }
+  };
+  xhr.send();
+  }, []);
+  
+  console.log(data);//for deubgging
+
+  return(
+    <div>
+      data
+    </div>
   );
 }
