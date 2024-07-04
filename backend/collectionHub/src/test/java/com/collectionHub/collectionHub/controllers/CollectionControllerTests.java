37a98f8bdd1e35562f0be9aaae9add1e678c25ba
mikeKaufman310 <mpk225@lehigh.edu>
2024-07-04 12:33:15 -0400

    started collection controller unit tests, figured out mocking jpa repo

---
+++ backend/collectionHub/src/test/java/com/collectionHub/collectionHub/controllers/CollectionControllerTests.java
@@ -0,0 +1,33 @@
+package com.collectionHub.collectionHub.controllers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.ArrayList;
+import static org.mockito.Mockito.when;
+import com.collectionHub.collectionHub.controllers.*;
+import com.collectionHub.collectionHub.entity.Item;
+import com.collectionHub.collectionHub.repository.ItemRepository;
+
+@WebMvcTest(CollectionController.class)
+class CollectionControllerTests{
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private ItemRepository itemRepo;
+
+    @Test
+    void testAllCollections() throws Exception{
+        when(itemRepo.findAll()).thenReturn(new ArrayList<>());
+
+        mockMvc.perform(get("/allCollections")).andExpect(status().isOk());
+    }
+}
