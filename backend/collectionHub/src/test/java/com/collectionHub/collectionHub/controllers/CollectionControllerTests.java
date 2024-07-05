f7a184299b0bea91efef958dbe846683934e6831
mikeKaufman310 <mpk225@lehigh.edu>
2024-07-05 11:55:55 -0400

    figured out mocking post requests in backend unit testing

--- backend/collectionHub/src/test/java/com/collectionHub/collectionHub/controllers/CollectionControllerTests.java
+++ backend/collectionHub/src/test/java/com/collectionHub/collectionHub/controllers/CollectionControllerTests.java
@@ -6,8 +6,10 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.http.MediaType;
 
 import java.util.ArrayList;
 import static org.mockito.Mockito.when;
@@ -27,7 +29,14 @@ class CollectionControllerTests{
     @Test
     void testAllCollections() throws Exception{
         when(itemRepo.findAll()).thenReturn(new ArrayList<>());
-
         mockMvc.perform(get("/allCollections")).andExpect(status().isOk());
+    }
+
+    @Test
+    void testGetCollection() throws Exception{
+        String collectionName = "collection";
+        when(itemRepo.findAll()).thenReturn(new ArrayList<>());
+        mockMvc.perform(get("/allCollections"));
+        mockMvc.perform(post("/getCollection").contentType(MediaType.APPLICATION_JSON).content(collectionName)).andExpect(status().isOk());
     }
 }
