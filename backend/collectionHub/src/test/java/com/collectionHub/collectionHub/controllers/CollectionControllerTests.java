a89775bdb41e49081e36ef81a44ebb68cd9c4001
mikeKaufman310 <mpk225@lehigh.edu>
2024-07-08 14:59:35 -0400

    completed controller unit tests

--- backend/collectionHub/src/test/java/com/collectionHub/collectionHub/controllers/CollectionControllerTests.java
+++ backend/collectionHub/src/test/java/com/collectionHub/collectionHub/controllers/CollectionControllerTests.java
@@ -11,11 +11,10 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import org.springframework.http.MediaType;
-//import com.fasterxml.jackson.databind.ObjectMapper;
-//import static org.mockito.ArgumentMatchers.any;
 
 import java.util.ArrayList;
 import static org.mockito.Mockito.when;
+import org.mockito.Mockito;
 import com.collectionHub.collectionHub.controllers.*;
 import com.collectionHub.collectionHub.entity.Item;
 import com.collectionHub.collectionHub.repository.ItemRepository;
@@ -64,23 +63,24 @@ class CollectionControllerTests{
         mockMvc.perform(post("/allCollections").contentType(MediaType.APPLICATION_JSON).content(collectionName)).andExpect(status().isOk());
     }
 
-    //@Test
-    //void testAddItemToCollection() throws Exception{
-    //    when(itemRepo.findAll()).thenReturn(new ArrayList<>());
-    //    mockMvc.perform(get("/allCollections"));
-    //    CollectionItem item = new CollectionItem("collection", "item", null, null, null, null, null);
-    //    Item itemItem = new Item();
-    //    itemItem.setcollection(item.collectionName());
-    //    itemItem.setName(item.name());
-    //    mockMvc.perform(post("/allCollections").contentType(MediaType.APPLICATION_JSON).content("collection")).andExpect(status().isOk());
-    //    when(itemRepo.save(itemItem)).thenReturn(itemItem);
-    //    mockMvc.perform(post("/addToCollections").contentType(MediaType.APPLICATION_JSON).content(
-    //        "{collection, name: \"name\", series: \"series\", number: 1, dateReleased: \"date\", dateOfAcquisition: \"data\", productionRun: 1}"
-    //    )).andExpect(status().isOk());
-    //}
+    @Test
+    void testAddItemToCollection() throws Exception{
+        when(itemRepo.findAll()).thenReturn(new ArrayList<>());
+        mockMvc.perform(get("/allCollections"));
+        CollectionItem item = new CollectionItem("collection", "item", null, null, null, null, null);
+        Item itemItem = new Item();
+        itemItem.setcollection(item.collectionName());
+        itemItem.setName(item.name());
+        mockMvc.perform(post("/allCollections").contentType(MediaType.APPLICATION_JSON).content("collection")).andExpect(status().isOk());
+        when(itemRepo.save(itemItem)).thenReturn(itemItem);
+        mockMvc.perform(post("/addToCollections").contentType(MediaType.APPLICATION_JSON).content("{\"collectionName\": \"ollectio\", \"name\": \"\", \"series\": \"\", \"number\": null, \"dateReleased\": \"\", \"dateOfAcquisition\": \"\", \"productionRun\": null}")).andExpect(status().isOk()); 
+    }
 
-    //@Test
-    //void testRemoveItem() throws Exception{
-    //    
-    //}
+    @Test
+    void testRemoveItem() throws Exception{
+        when(itemRepo.findAll()).thenReturn(new ArrayList<>());
+        mockMvc.perform(get("/allCollections"));
+        mockMvc.perform(post("/allCollections").contentType(MediaType.APPLICATION_JSON).content("collection")).andExpect(status().isOk());
+        mockMvc.perform(delete("/deleteFromCollections").contentType(MediaType.APPLICATION_JSON).content("{\"collectionName\": \"ollectio\", \"name\": \"\", \"series\": \"\", \"number\": null, \"dateReleased\": \"\", \"dateOfAcquisition\": \"\", \"productionRun\": null}")).andExpect(status().isOk());
+    }
 }
