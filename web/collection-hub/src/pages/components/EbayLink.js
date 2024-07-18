bc6e98f9b81a93c16c1360ab78466903f5bf0fd5
mikeKaufman310 <mpk225@lehigh.edu>
2024-07-18 10:07:45 -0400

    added idea about design flow of ebay component

--- web/collection-hub/src/pages/components/EbayLink.js
+++ web/collection-hub/src/pages/components/EbayLink.js
@@ -4,6 +4,8 @@ export default function EbayLink(){
     //query open ai using list of elements in collection already to get good ebay search
     //hit ebay api to get link of search results
     //render search result link in return html of react component
+    
+    //QUESTION: should we save the queries in a backend repository for reuse?
     return(
         <div></div>
     );
