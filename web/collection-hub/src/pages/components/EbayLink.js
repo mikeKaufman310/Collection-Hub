6327b103e4d5e75fa7efb6c2bff3cf91deb617b0
mikeKaufman310 <mpk225@lehigh.edu>
2024-07-18 10:06:47 -0400

    initial in code design of ebay recommendation component

---
+++ web/collection-hub/src/pages/components/EbayLink.js
@@ -0,0 +1,10 @@
+
+//this component will be used in Collection component
+export default function EbayLink(){
+    //query open ai using list of elements in collection already to get good ebay search
+    //hit ebay api to get link of search results
+    //render search result link in return html of react component
+    return(
+        <div></div>
+    );
+}
---
+++ web/collection-hub/src/pages/components/EbayLink.test.js
@@ -0,0 +1,6 @@
+
+//test that ebay link is present on collection component render
+
+//test that link is clickable and change page state
+
+//jest test that out going api call to ebay and open ai happens
