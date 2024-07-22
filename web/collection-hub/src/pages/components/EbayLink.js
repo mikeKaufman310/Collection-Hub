e54bd4b6879064a1e62e857e1cff5678443c231f
mikeKaufman310 <mpk225@lehigh.edu>
2024-07-22 15:08:31 -0400

    integrated groq instead of open ai, still need to debug

--- web/collection-hub/src/pages/components/EbayLink.js
+++ web/collection-hub/src/pages/components/EbayLink.js
@@ -1,11 +1,12 @@
 import { useLocation } from "react-router-dom";
 import { useState } from "react";
-import OpenAI from "openai";
+import Groq from "groq-sdk";
 import axios from 'axios';
 
 const BACKEND_PORT = 8080;
-//const ai = new OpenAI({dangerouslyAllowBrowser: true});
-const ai = new OpenAI();
+//const ai = new OpenAI();
+const ai = new Groq({apiKey:'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', dangerouslyAllowBrowser:true});
+//console.log(process.env.GROQ_API_KEY);
 //IMPORTANT: might need to change to groq to be free
 
 //this component will be used in Collection component
@@ -17,18 +18,23 @@ export default function EbayLink(){
     const [searchStr, setSearchStr] = useState('');
     const [searchResults, setSearchResults] = useState([]);
     const [trigger, setTrigger] = useState(false);
+    const [aiTrig, setAiTrig] = useState(false);
     //query backend for list of item already contained
     const xhr = new XMLHttpRequest();
     xhr.open('POST', `http://localhost:${BACKEND_PORT}/getCollection`, true);
     xhr.onreadystatechange = function(){
         if(this.readyState===XMLHttpRequest.DONE){
             if(xhr.status===200){
-                console.log(xhr.responseText);
+                //console.log(xhr.responseText);
                 if(!trigger){
                     setData(JSON.parse(xhr.responseText));
                     setTrigger(true);
+                    if(!aiTrig){
+                        aiQuery();
+                        setAiTrig(true);
+                    }
                 }
-            return;
+                return;
             }
         }else{
             console.error(xhr.statusText);
@@ -37,15 +43,18 @@ export default function EbayLink(){
     xhr.send(JSON.stringify(collectionName));
     console.log("Received " + collectionName + " data for open ai use");
     //query open ai using list of elements in collection already to get good ebay search
-    const openAiQuery = async () =>{
+    const aiQuery = async () =>{
+        let contentStr = `Give me an example of an ebay search for ${collectionName} products that does not contain`;
+        for(let i = 0; i < data.collectionList.length; i++){
+            contentStr+=(" " + data.collectionList[i].name);
+        }
         const search = await ai.chat.completions.create({
-            messages: [{ role: "system", content: `Give me an ebay search for ${collectionName} products that does not contain ${data}` }],//use data state here for query
-            model: "gpt-3.5-turbo",
+            messages: [{ role: "user", content: `${contentStr}` }],//use data state here for query
+            model: "llama3-8b-8192",
         });
         setSearchStr(search.choices[0]);
-        console.log(search.choices[0]);//for debugging
+        console.log(search.choices[0].message.content);//for debugging
     };
-    openAiQuery();
     //hit ebay api to get link of search results
     const ebaySearch = async () => {
         const response = await axios.get('https://api.ebay.com/buy/browse/v1/item_summary/search', {
