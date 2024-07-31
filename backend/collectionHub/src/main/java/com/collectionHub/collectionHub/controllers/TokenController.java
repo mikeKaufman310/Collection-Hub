cd96c480b735e8ec8d7eadd78f730ab3ab58d586
mikeKaufman310 <mpk225@lehigh.edu>
2024-07-31 10:49:39 -0400

    added rest template for internal rest calls

--- backend/collectionHub/src/main/java/com/collectionHub/collectionHub/controllers/TokenController.java
+++ backend/collectionHub/src/main/java/com/collectionHub/collectionHub/controllers/TokenController.java
@@ -1,9 +1,15 @@
 package com.collectionHub.collectionHub.controllers;
 
 import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class TokenController{
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @GetMapping("/getEbayToken")
     public String getEbayToken(){
@@ -21,6 +27,8 @@ public class TokenController{
                 }
          */
         //return ebay response credential as response value
+        String url = "https://api.ebay.com/identity/v1/oauth2/token";//no params
+        //https://api.ebay.com/identity/v1/oauth2/token?grant_type=client_credentials&scope=https://api.ebay.com/oauth/api_scope
         return "";
     }
 }
